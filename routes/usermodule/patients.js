3e2b66b7fbdf089aa9dbc22f08eaab2528b9f5fb
Ahmet Kaymak <ahmet.kaymak@stu.fsm.edu.tr>
2017-03-12 18:58:10 +0300

    V.0
    Retrofit'in kanser ettiği anlar

--- routes/usermodule/patients.js
+++ routes/usermodule/patients.js
@@ -9,8 +9,8 @@ var router = express.Router();
 
 //get patient profile data data
 
-router.post('/patients/getPatientProfile', function (req, res, next) {
-    var user_id = req.body.user_id;
+router.get('/patients/getPatientProfile/:user_id', function (req, res, next) {
+    var user_id = req.params.user_id;
     models.USERS.findById(user_id, {
         attributes: ['user_id','user_name','about_me','friend_count','is_official_user','is_official_user','profile_picture_id'],
         model: models.USERS,
