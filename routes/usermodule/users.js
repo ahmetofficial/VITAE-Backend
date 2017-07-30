63d3680dae5e9bb798895f3a3c4402f5a5278f36
Ahmet Kaymak <ahmet.kaymak@stu.fsm.edu.tr>
2017-07-30 23:42:40 +0300

    V.0
    Kullanıcı genel bilgilerini güncellem ile ilgili fonksiyonlar eklendi

--- routes/usermodule/users.js
+++ routes/usermodule/users.js
@@ -12,14 +12,6 @@ var env = process.env.NODE_ENV || 'development';
 var config = require(__dirname + '/../../config/config.json')[env];
 var sequelize = new Sequelize(config.database, config.username, config.password, config);
 
-//getAllTheUserData
-router.get('/users/getAll', function (req, res, next) {
-    models.USERS.findAll()
-        .then(function (USERS) {
-            res.send({data: USERS});
-        })
-});
-
 //get users friends
 router.get('/users/getFriends/:user_id', function (req, res, next) {
     var user_id = req.params.user_id;
@@ -78,7 +70,6 @@ router.get('/users/followCount/:active_user_id', function (req, res, next) {
     });
 });
 
-
 //update user about me
 router.post('/users/updateUserAboutMe', function (req, res, next) {
     var user_id = req.body.user_id;
@@ -98,7 +89,6 @@ router.post('/users/updateUserAboutMe', function (req, res, next) {
         res.status(500).json(error)
     });
 });
-
 
 //login authentication
 router.post('/users/login', function (req, res) {
@@ -209,17 +199,16 @@ router.post('/users/registerPatient', function (req, res) {
 
 //changing user password
 router.put('/users/resetPassword/:user_id', function (req, res) {
-    var old_password = req.body.old_password;
-    var new_password = req.body.new_password;
+    var password = req.body.password;
+    var user_id = req.params.user_id;
     models.USERS.find({
         where: {
-            user_id: req.params.user_id,
-            password: old_password
+            user_id: user_id
         }
     }).then(function (USERS) {
-        if (USERS && old_password != new_password) {
+        if (USERS) {
             USERS.updateAttributes({
-                password: new_password
+                password: password
             }).then(function (USERS) {
                 res.status(200).json({
                     status: 'true',
@@ -228,14 +217,7 @@ router.put('/users/resetPassword/:user_id', function (req, res) {
             }).catch(function (error) {
                 res.status(500).json(error)
             });
-        }
-        else if (USERS && old_password == new_password) {
-            res.status(200).json({
-                status: 'false',
-                message: 'Please enter different password from current password'
-            });
-        }
-        else {
+        } else {
             res.status(200).json({
                 status: 'false',
                 message: 'Password has not changed, try again!'
@@ -244,47 +226,136 @@ router.put('/users/resetPassword/:user_id', function (req, res) {
     });
 });
 
-//changing user_id
-router.put('/users/resetUserId/:user_id', function (req, res) {
-    var old_user_id = req.params.user_id;
-    var new_user_id = req.body.new_user_id;
+//check user_id avaibility
+router.get('/users/isUserIdAvaible/:user_id', function (req, res) {
+    var user_id = req.params.user_id;
     models.USERS.find({
         where: {
-            user_id: new_user_id
+            user_id: user_id
         }
     }).then(function (USERS) {
-        if (isNaN(USERS) && old_user_id != new_user_id) {
+        if (USERS == null) {
+            res.status(200).json({
+                status: 'true'
+            });
+        } else {
+            res.status(200).json({
+                status: 'false'
+            });
+        }
+    }).catch(function (error) {
+        res.status(500).json(error)
+    });
+});
+
+//changing mail
+router.put('/users/resetMail/:user_id', function (req, res) {
+    var user_id = req.params.user_id;
+    var mail = req.body.mail;
+    models.USERS.find({
+        where: {
+            user_id: user_id
+        }
+    }).then(function (USERS) {
+        if (USERS) {
             USERS.updateAttributes({
-                user_id: new_user_id
+                mail: mail
             }).then(function (USERS) {
                 res.status(200).json({
                     status: 'true',
-                    message: 'User id has changed successfully'
+                    message: 'Mail has changed successfully'
                 });
             }).catch(function (error) {
                 res.status(500).json(error)
             });
-        }
-        else if (isNaN(USERS) && old_user_id == new_user_id) {
+        } else {
             res.status(200).json({
                 status: 'false',
-                message: 'Please enter different user_id from current user_id'
-            });
-        }
-        else {
-            res.status(200).json({
-                status: 'false',
-                message: 'It has already taken'
+                message: 'Mail name hasn\'t changed successfully'
             });
         }
     });
 });
 
+//changing user_name
+router.put('/users/resetUserName/:user_id', function (req, res) {
+    var user_id = req.params.user_id;
+    var user_name = req.body.user_name;
+    models.USERS.find({
+        where: {
+            user_id: user_id
+        }
+    }).then(function (USERS) {
+        if (USERS) {
+            USERS.updateAttributes({
+                user_name: user_name
+            }).then(function (USERS) {
+                res.status(200).json({
+                    status: 'true',
+                    message: 'User name has changed successfully'
+                });
+            }).catch(function (error) {
+                res.status(500).json(error)
+            });
+        } else {
+            res.status(200).json({
+                status: 'false',
+                message: 'User name hasn\'t changed successfully'
+            });
+        }
+    });
+});
+
+//changing about_me
+router.put('/users/resetAboutMe/:user_id', function (req, res) {
+    var user_id = req.params.user_id;
+    var about_me = req.body.about_me;
+    models.USERS.find({
+        where: {
+            user_id: user_id
+        }
+    }).then(function (USERS) {
+        if (USERS) {
+            USERS.updateAttributes({
+                about_me: about_me
+            }).then(function (USERS) {
+                res.status(200).json({
+                    status: 'true',
+                    message: 'About me has changed successfully'
+                });
+            }).catch(function (error) {
+                res.status(500).json(error)
+            });
+        } else {
+            res.status(200).json({
+                status: 'false',
+                message: 'About me hasn\'t changed successfully'
+            });
+        }
+    });
+});
+
+//search by user_id
 router.post('/users/searchByUserInfo', function (req, res, next) {
     var search_text = req.body.search_text;
     sequelize.query('SELECT user_id,user_name, user_type_id, is_official_user,profile_picture_id FROM USERS WHERE MATCH(user_name) AGAINST("' + search_text + '"IN NATURAL LANGUAGE MODE);'
     ).then(function (USERS) {
         res.send({users: USERS[0]});
+    }).catch(function (error) {
+        res.status(500).json(error)
+    });
+});
+
+//get user information by user_id
+router.get('/users/getUserInformation/:user_id', function (req, res, next) {
+    var user_id = req.params.user_id;
+    models.USERS.findById(user_id, {
+        model: models.USERS,
+        where: {
+            user_id: user_id
+        }
+    }).then(function (USERS) {
+        res.send(USERS);
     }).catch(function (error) {
         res.status(500).json(error)
     });
