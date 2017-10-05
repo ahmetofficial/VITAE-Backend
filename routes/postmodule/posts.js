f9a057184ce27058aa64e46b2116f457d0c8c9de
Ahmet Kaymak <ahmet.kaymak@stu.fsm.edu.tr>
2017-10-06 01:48:53 +0300

    V.0
    Bug fix

--- routes/postmodule/posts.js
+++ routes/postmodule/posts.js
@@ -55,7 +55,7 @@ router.get('/posts/liveFeed/:user_id', function (req, res, next) {
     models.USER_POST.findAll({
         include: [
             {
-                attributes: ['user_name'],
+                attributes: ['user_name','profile_picture_id'],
                 model: models.USERS,
                 include: [
                     {
