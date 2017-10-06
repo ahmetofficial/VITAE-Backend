0096fdc116f5dfe30572298634cb319c7521128b
Ahmet Kaymak <ahmet.kaymak@stu.fsm.edu.tr>
2017-10-06 23:53:30 +0300

    V.0
    Bug Fix

--- routes/postmodule/posts.js
+++ routes/postmodule/posts.js
@@ -21,6 +21,10 @@ router.get('/posts/getByUserId/:user_id', function (req, res, next) {
             {
                 attributes: ['post_id', 'user_id', 'created_at'],
                 model: models.USER_POST_LIKE
+            },
+            {
+                attributes: ['post_id', 'photo_id'],
+                model: models.USER_POST_HAVE_PHOTOS
             }
         ],
         where: {
