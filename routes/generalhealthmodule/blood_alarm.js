0499f9d5d7f213aa0e00802cd75f8d0fd1634b32
Ahmet Kaymak <ahmet.kaymak@stu.fsm.edu.tr>
2017-10-16 00:42:36 +0300

    V.0
    Bug fixes

--- routes/generalhealthmodule/blood_alarm.js
+++ routes/generalhealthmodule/blood_alarm.js
@@ -7,6 +7,16 @@ var models = require('../../models');
 var express = require('express');
 var router = express.Router();
 const uuidv1 = require('uuid/v1');
+var Sequelize = require('sequelize');
+var env = process.env.NODE_ENV || 'development';
+var config = require(__dirname + '/../../config/config.json')[env];
+var sequelize = new Sequelize(config.database, config.username, config.password, config);
+
+var serverKey = "AAAATo6KBmk:APA91bH90b6T6ta3vTC58SpfJGlftEDofRcba1H0SFQT_Z85WBSM69AAllBTbiWqdlXc-X-8lFzcdzCmtCyDXJYcPwavrvVNu3bNxd7ub2r_CATjLWM68HMNxTYEiTFxipNAnx7ki6nT";
+var FCM = require('fcm-node');
+var fcm = new FCM(serverKey);
+
+var bloodTypes = ['undefined', '0 Rh+', 'O Rh-', 'A Rh+', 'A Rh-', 'B Rh+', 'B Rh-', 'AB Rh+', 'AB Rh-'];
 
 //create blood alarm
 router.post('/bloodAlarm/create', function (req, res, next) {
@@ -24,38 +34,58 @@ router.post('/bloodAlarm/create', function (req, res, next) {
         alarm_level: alarm_level,
         contact_number: contact_number
     }).then(function () {
+        models.HOSPITALS.findAll({
+            where: {
+                hospital_id: hospital_id
+            }
+        }).then(function (HOSPITALS) {
+            var hospital_name = HOSPITALS[0].hospital_name;
+            var hospital_latitude = HOSPITALS[0].latitude;
+            var hospital_longitude = HOSPITALS[0].longitude;
+            //sendPushNotification(hospital_name,hospital_latitude,hospital_longitude,blood_type_id,res);
+            sequelize.query(
+                ' SELECT USER_LOCATION.user_id,USERS.device_token,( 6371 * acos( cos( radians(' + hospital_latitude + ') ) ' +
+                ' * cos( radians( USER_LOCATION.latitude ) ) ' +
+                ' * cos( radians( USER_LOCATION.longitude ) - radians(' + hospital_longitude + ') ) ' +
+                ' + sin( radians(' + hospital_latitude + ') ) ' +
+                ' * sin( radians( USER_LOCATION.latitude ) ) ) ) AS distance ' +
+                ' FROM USER_LOCATION ' +
+                ' JOIN USERS ' +
+                ' ON USERS.user_id=USER_LOCATION.user_id ' +
+                ' JOIN PATIENTS ' +
+                ' ON USERS.user_id=PATIENTS.user_id ' +
+                ' WHERE PATIENTS.blood_type_id=' + blood_type_id + ' ' +
+                ' HAVING distance < 10 ',
+                {type: sequelize.QueryTypes.SELECT}
+            ).then(function (USERS) {
+                for (var i in USERS) {
+                    var token = USERS[i].device_token;
+                    var message = {
+                        to: token,
+                        notification: {
+                            title: bloodTypes[blood_type_id],
+                            body: hospital_name
+                        }
+                    };
+                    fcm.send(message, function (err, response) {
+                        if (err) {
+                            //console.log("Notification not sent");
+                        } else {
+                            //console.log("Successfully sent with response: ", response);
+                        }
+                    });
+                }
+            }).catch(function (error) {
+
+            });
+        }).catch(function (error) {
+            res.status(500).json(error)
+        });
         res.send({status: true});
     }).catch(function (error) {
         res.status(500).json(error)
     });
 });
-
-/*
-function send() {
-    var message = "Hey! you got this notification.";
-    var title = "DigitSTORY Notification";
-    var token = "<Your Device Token for Android>" *;
-    var message = {
-        to: token,
-        notification: {
-            title: "Kan Alarmı", //title of notification
-            body: "A RH+ kana ihityaç var", //content of the notification
-            sound: "default"
-        },
-        data: data //payload you want to send with your notification
-    };
-    fcm.send(message, function (err, response) {
-        if (err) {
-            console.log("Notification not sent");
-            res.json({success: false})
-        } else {
-            console.log("Successfully sent with response: ", response);
-            res.json({success: true})
-        }
-    });
-
-}
-*/
 
 //get blood alarms
 router.get('/bloodAlarm/getAllBloodAlarms', function (req, res, next) {
